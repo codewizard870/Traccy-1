81130e81b9e69ab99750ccd87c78902c4c95c412
Eric <ericleee777@gmail.com>
2023-05-12 06:38:20 -0400

    intro first responsive

--- src/containers/ImpactThroughTraccy/index.js
+++ src/containers/ImpactThroughTraccy/index.js
@@ -116,7 +116,7 @@ const ImpactThroughTraccy = () => {
                         // style={{ backgroundImage: `url("/impact/intro-1-background.png")` }}
                         >
                            <div className="page-1-left">
-                              <h1>Impact<br />Through<br />Traccy </h1>
+                              <h1>Impact Through Traccy</h1>
                               <p>
                                  At Traccy, it is all about creating impact. We have built an ecosystem that enables changemakers to invest quickly and securely in sustainable innovations, while also providing qualified project owners with the financing they need. In doing so, we're bridging the gap between funds seeking projects and projects ready to be funded on a global scale.
                               </p>
