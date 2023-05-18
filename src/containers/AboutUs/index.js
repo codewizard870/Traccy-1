6260832f6597d1543bbbcbf0e9b72f7fe0129444
bluecat870 <alenzer0902@gmail.com>
2023-05-18 04:02:57 -0400

    update video property

--- src/containers/AboutUs/index.js
+++ src/containers/AboutUs/index.js
@@ -227,7 +227,7 @@ const AboutUs = () => {
                                 {/* <div className='play-icon'><SvgIcon name='play' viewbox='0 0 23 30.001' /></div> */}
                                 {/* <img src={videoimg} alt='video-img' /> */}
                                 <iframe src="https://www.youtube.com/embed/aZx5E3QVL0M" title="video"
-                                    width="100%" style={{ height: "43vw" }}></iframe>
+                                    width="100%" allowTransparency={true} style={{ height: "52vw", border: "0px" }}></iframe>
                                 {/* <iframe width="420" height="315"
                                     src="https://www.youtube.com/embed/tgbNymZ7vqY?playlist=tgbNymZ7vqY&loop=1">
                                 </iframe> */}
