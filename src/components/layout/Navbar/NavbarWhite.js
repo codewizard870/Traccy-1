679e4a8ce55b221f7ca9c6348e668e69606d9740
bluecat870 <alenzer0902@gmail.com>
2023-05-19 03:57:09 -0400

    conver lowercase on navbar

--- src/components/layout/Navbar/NavbarWhite.js
+++ src/components/layout/Navbar/NavbarWhite.js
@@ -49,9 +49,9 @@ const Navbar = () => {
             <img src={TraccyLogo} alt="logo" />
             <img src={TraccyLogoSmall} alt="logo" />
             <p>
-              Using <br />
-              Blockchain <br />
-              Impactful
+              using <br />
+              blockchain <br />
+              impactful
             </p>
           </div>
           <div className="right-header">
